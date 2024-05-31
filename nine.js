ef01cf5777954a1683b1e5bb63ce19a5d6d0e908
aladalatv <167577215+aladalatv@users.noreply.github.com>
2024-05-31 15:12:35 +0700

    Create nine.js

---
+++ nine.js
@@ -0,0 +1,7 @@
+function override(url){ if (url.indexOf("licensing.bitmovin.com/licensing") > -1) return "data:text/plain;charset=utf-8;base64,eyJzdGF0dXMiOiJncmFudGVkIiwibWVzc2FnZSI6IlRoZXJlIHlvdSBnby4ifQ=="; if (url.indexOf("licensing.bitmovin.com/impression") > -1) return "data:text/plain;charset=utf-8;base64,eyJzdGF0dXMiOiJncmFudGVkIiwibWVzc2FnZSI6IlRoZXJlIHlvdSBnby4ifQ=="; return url;}
+var opens = XMLHttpRequest.prototype.open;XMLHttpRequest.prototype.open = function() {var url = override(arguments[1]);arguments[1] = url;return opens.apply(this, arguments);}
+var config = {key: "11d3698c-efdf-42f1-8769-54663995de2b",playback: {autoplay:true,muted: false,}};
+var source = {'hls':'https://tonton-live-ssai.akamaized.net/live/2020783a-0303-401a-9c92-b7c3c9108c0b/cmaf.isml/.m3u8'};
+var playerContainer = document.getElementById('playerdiv');
+var player = new bitmovin.player.Player(playerContainer, config);
+player.load(source).then(function() {player.play();console.log();});
